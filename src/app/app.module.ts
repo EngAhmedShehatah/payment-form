488c2559966b86dc3cfc9e574d4abfd78923814c
EngAhmedShehatah <engahmedshehatah@gmail.com>
2020-12-15 21:03:08 +0200

    Stephen Grider 4th app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ConvertMilesKmPipe } from './convert-miles-km.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConvertMilesKmPipe
   ],
   imports: [
     BrowserModule
---
+++ src/app/convert-miles-km.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ConvertMilesKmPipe } from './convert-miles-km.pipe';
+
+describe('ConvertMilesKmPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ConvertMilesKmPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
